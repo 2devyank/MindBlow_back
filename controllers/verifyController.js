26bce5662e37ec85e88891ed11ad42b8bd7fe134
devyank <devyanknagpal2002@gmail.com>
2024-06-29 13:20:19 +0530

    api for fetching and verifying otp added

--- controllers/verifyController.js
+++ controllers/verifyController.js
@@ -1,7 +1,7 @@
 const client=require('twilio')(process.env.Account_SID,process.env.AuthToken);
 
 exports.getCode=async(req,res)=>{
-    await client.verify.v2.services("VAd9f009e4912b7a4cbd29257ad1a3f40e")
+    await client.verify.v2.services(process.env.Service)
     .verifications.create({
         channel:"sms",
         to:"+918448137331",
@@ -11,7 +11,7 @@ exports.getCode=async(req,res)=>{
     
 }
 exports.verifyCode=async(req,res)=>{
-client.verify.v2.services('VAd9f009e4912b7a4cbd29257ad1a3f40e')
+client.verify.v2.services(process.env.Service)
 .verificationChecks
 .create({
     to:"+918448137331",
