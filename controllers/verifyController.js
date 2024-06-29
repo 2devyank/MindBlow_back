670ac01eda0a9b274d08ed8e7394a4019be0c93e
devyank <devyanknagpal2002@gmail.com>
2024-06-29 14:04:01 +0530

    authentication error corrected

--- controllers/verifyController.js
+++ controllers/verifyController.js
@@ -1,23 +1,23 @@
-const client=require('twilio')(process.env.Account_SID,process.env.AuthToken);
+const client = require('twilio')(process.env.Account_SID, process.env.AuthToken);
 
-exports.getCode=async(req,res)=>{
-    await client.verify.v2.services(process.env.Service)
-    .verifications.create({
-        channel:"sms",
-        to:"+918448137331",
-    }).then(data=>{
-        res.status(200).send(data.sid);
-    })
-    
+exports.getCode = async (req, res) => {
+    await client.verify.v2.services('VAd9f009e4912b7a4cbd29257ad1a3f40e')
+        .verifications.create({
+            channel: "sms",
+            to: "+918448137331",
+        }).then(data => {
+            res.status(200).send(data.sid);
+        })
+
 }
-exports.verifyCode=async(req,res)=>{
-client.verify.v2.services(process.env.Service)
-.verificationChecks
-.create({
-    to:"+918448137331",
-    code:req.query.code
-})
-.then(data=>{
-    res.status(200).send(data);
-})
+exports.verifyCode = async (req, res) => {
+    client.verify.v2.services(process.env.Service)
+        .verificationChecks
+        .create({
+            to: "+918448137331",
+            code: req.query.code
+        })
+        .then(data => {
+            res.status(200).send(data);
+        })
 }
